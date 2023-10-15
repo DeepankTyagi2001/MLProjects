bca9e0c436fefd40f29c119ed64d39e9752fc060
Deepank Tyagi <deepanktyagi2001@gmail.com>
2023-10-15 10:09:28 +0530

    forgot to add return to the reuirements which was restricting the creation of requires.txt file

--- setup.py
+++ setup.py
@@ -11,16 +11,17 @@ def get_requirements(file_path:str)->List[str]:
     with open (file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements ]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-        requirements
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
     name='mlproject',
     version='0.0.1',
     author="DeepankTyagi",
     author_email="deepanktyagi2001@gmail.com",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     install_requires=get_requirements('requirements.txt')
+    # install_requires=['pandas','numpy','seaborn']
 
 )
